2f82ba100ce75c874b1e2d12ba1e245c0ffe4181
Brian Caffey <bcaffey@drydengroup.com>
2018-10-05 20:29:48 -0400

    added additional chart formatting

--- frontend/src/views/Charts.vue
+++ frontend/src/views/Charts.vue
@@ -11,10 +11,50 @@ export default {
   name: 'Documents',
   data() {
     return {
+      legend: {
+        navigation: {
+          activeColor: '#fff',
+        },
+        itemStyle: {
+          color: '#fff',
+        },
+      },
+      labels: {
+        style: {
+          color: '#ff',
+        },
+      },
       chartOptions: {
-        series: [{
-          data: this.fetchData(),
-        }],
+        borderWidth: 0,
+        credits: {
+          enabled: false,
+        },
+        title: {
+          style: {
+            color: '#fff',
+          },
+        },
+        chart: {
+          xAxis: {
+            labels: {
+              style: {
+                color: '#fff',
+              },
+            },
+          },
+          type: 'bar',
+          backgroundColor: '#2c3e50',
+        },
+        colors: ['#910000', '#1aadce', '#492970', '#f28f43', '#77a1e5', '#c42525', '#a6c96a'],
+        series: [
+          {
+            data: this.fetchData(),
+            name: 'Hmm',
+          },
+          {
+            data: this.fetchData(),
+          },
+        ],
       },
     };
   },
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -30,7 +30,7 @@ export default {
   computed: {
     buttonCanClick() {
       return false;
-    }
+    },
   },
   methods: {
     login() {
